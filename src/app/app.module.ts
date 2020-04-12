a091a84166492ad842af10758167d370fca1607e
ahmedzaher <azaher@uplandsoftware.com>
2020-04-12 17:49:07 +0200

    install ng material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,17 +5,21 @@ import {HttpClientModule} from '@angular/common/http'
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { StoreComponent } from './store/store.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+//import { MatSliderModule } from '@angular/material/slider';
 
 @NgModule({
   declarations: [
     AppComponent,
-    StoreComponent
+    StoreComponent    
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule
-  ],
+    HttpClientModule,
+    BrowserAnimationsModule
+],
   providers: [],
   bootstrap: [AppComponent]
 })
